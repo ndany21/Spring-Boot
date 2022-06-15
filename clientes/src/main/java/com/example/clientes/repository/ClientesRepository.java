4d2739615d22225d51492c84fd97fbe605fedbba
ndany21 <100426943+ndany21@users.noreply.github.com>
2022-06-15 23:49:49 +0200

    Delete clientes directory

--- clientes/src/main/java/com/example/clientes/repository/ClientesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.clientes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.clientes.model.Clientes;
-
-public abstract interface ClientesRepository  extends JpaRepository<Clientes, Long>{
-
-}
